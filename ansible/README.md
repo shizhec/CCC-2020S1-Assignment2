282b9cda9ecac869ae49c53decfe4ba7c3779bf6
shizhec <shizhec@shizhec-ubuntu18.04>
2020-04-25 02:00:42 +1000

    initial necter setup script

---
+++ ansible/README.md
@@ -0,0 +1,5 @@
+# Run Ansible
+## The Openstack Password
+```
+ODQ2MGVlOGVhODhjNjhh
+```
---
+++ ansible/setup-nectar.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+. ./unimelb-comp90024-2020-grp-12-openrc.sh; ansible-playbook --ask-become-pass setup_nectar.yaml
