ede30994561470671414c644b48ecbaa477d6293
Yizhou Zhu <yizzhu@student.unimelb.edu.au>
2020-05-06 22:25:51 +1000

    script for scraping tweets from specific locations and period. Use the modified GetOldTweets3 , no pip install

--- scraper/searching/getFromUsername.py
+++ scraper/searching/getFromUsername.py
@@ -8,14 +8,14 @@ parser.add_argument('--username', type=str, default="realDonaldTrump")
 parser.add_argument('--startdate', type=str, default="2020-01-01")
 parser.add_argument('--enddate', type=str, default="2020-04-29")
 parser.add_argument('--within', type=str, default="50mi")
-parser.add_argument('--max', type=int, default=500)
+parser.add_argument('--max', type=int, default=0)
 args = parser.parse_args()
 
 tweetCriteria = got.manager.TweetCriteria()\
                 .setUsername(args.username)\
                 .setSince(args.startdate)\
                 .setUntil(args.enddate)\
-                .setMaxTweets(args.max)
+              
 
 tweetlist = got.manager.TweetManager.getTweets(tweetCriteria)
 
@@ -24,12 +24,22 @@ print(len(tweetlist))
 couch = couchdb.Server('http://90024_12:90024_12@localhost:5984')
 db = couch.create('trump_sample')
 
-# for tweet in tweetlist:
-#     temp = {}
-#     temp['id'] = tweet.id
-#     temp['permalink'] = tweet.permalink
-#     temp['username']=tweet.username
-#     temp['text']= tweet.text
-#     temp['hashtags'] = tweet.hashtags
-#     temp['geo'] = tweet.geo
-#     db.save(temp)
+for tweet in tweetlist:
+    temp = {}
+    temp['id'] = tweet.id
+    temp['permalink'] = tweet.permalink
+    temp['username']=tweet.username
+    temp["date"] = tweet.date.strftime('%Y-%m-%d %H:%M:%S%z')
+    temp['text']= tweet.text
+    temp['hashtags'] = tweet.hashtags.split()
+    temp['geo'] = [tweet.geo]
+    db.save(temp)
+
+
+function (doc) {
+  doc.hashtags.forEach(getHashtag);
+  
+  function getHashtag(item) {
+  emit(item, 1);
+  }
+}
--- scraper/searching/getfromLocaiton.py
+++ scraper/searching/getfromLocaiton.py
@@ -10,20 +10,23 @@ parser = argparse.ArgumentParser(description='COMP90024 / get tweet via coordina
 parser.add_argument('--coordinates', type=str, default="-37.80811,144.96071")
 parser.add_argument('--startdate', type=str, default="2020-04-28")
 parser.add_argument('--enddate', type=str, default="2020-04-29")
-parser.add_argument('--within', type=str, default="1mi")
-parser.add_argument('--max', type=int, default=1000)
+parser.add_argument('--within', type=str, default="50mi")
+parser.add_argument('--max', type=int, default=100)
+parser.add_argument('--filename', type=str, default="get_geo_tweet.txt")
 args = parser.parse_args()
 
 START_MSG = "1"
 ENG_MSG = "0"
+
+output = open(args.filename, "w")
 
 # comm = MPI.COMM_WORLD
 # comm_size = comm.Get_size()
 # comm_rank = comm.Get_rank()
 
 
-couch = couchdb.Server('http://90024_12:90024_12@localhost:5984')
-db = couch['twitter_sample']
+# couch = couchdb.Server('http://90024_12:90024_12@localhost:5984')
+# db = couch['twitter_sample']
 
 # tweet within 50mil of Melbourne central located 
 tweetCriteria = got.manager.TweetCriteria()\
@@ -33,17 +36,18 @@ tweetCriteria = got.manager.TweetCriteria()\
                 .setUntil(args.enddate)\
                 .setMaxTweets(args.max)
 
-tweetlist = got.manager.TweetManager.getTweets(tweetCriteria)
+got.manager.TweetManager.getTweets(tweetCriteria,file = output)
 
-print(tweetlist[0].geo)
+# print(len(tweetlist))
 
-for tweet in tweetlist:
-    temp = {}
-    temp['id'] = tweet.id
-    temp['permalink'] = tweet.permalink
-    temp['username']=tweet.username
-    temp['text']= tweet.text
-    temp['hashtags'] = tweet.hashtags
-    temp['geo'] = tweet.geo
-    db.save(temp)
+# for tweet in tweetlist:
+#     temp = {}
+#     temp['id'] = tweet.id
+#     temp['permalink'] = tweet.permalink
+#     temp['username']=tweet.username
+#     temp["date"] = tweet.date.strftime('%Y-%m-%d %H:%M:%S%z')
+#     temp['text']= tweet.text
+#     temp['hashtags'] = tweet.hashtags.split()
+#     temp['geo'] = [tweet.geo]
+#     db.save(temp)
 
