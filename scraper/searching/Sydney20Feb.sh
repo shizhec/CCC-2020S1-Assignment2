27030f45953b84dffc5a28eaf4a766622042ecf1
Peng Cao <45455020+c731615340@users.noreply.github.com>
2020-05-07 16:16:11 +1000

    Update Sydney20Feb.sh

--- scraper/searching/Sydney20Feb.sh
+++ scraper/searching/Sydney20Feb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python getfromLocation.py  --startdate 2020-02-01 --enddate 2020-03-01 --coordinates -33.865143,151.209900 --filename Sydney20_Feb.txt
+python getSydney.py  --startdate 2020-02-01 --enddate 2020-03-01 --filename Sydney20_Feb.txt
