b3ce9111bb26a251f87035e62e2334f1383767ae
shizhec <shizhec@shizhec-ubuntu18.04>
2020-05-07 19:22:32 +1000

    change python to python3

--- scraper/searching/Sydney20Mar.sh
+++ scraper/searching/Sydney20Mar.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python getSydney.py  --startdate 2020-03-01 --enddate 2020-04-01 --filename Sydney20_Mar.txt
+python3 getSydney.py  --startdate 2020-03-01 --enddate 2020-04-01 --filename Sydney20_Mar.txt
