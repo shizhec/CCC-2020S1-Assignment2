3e7c88aace544fc58b532c36d9236f80ac36260e
Peng Cao <45455020+c731615340@users.noreply.github.com>
2020-05-22 23:59:18 +1000

    Create TopicModelling.py

---
+++ analysis/TopicModelling.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import json
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+import sys
+import re, numpy as np, pandas as pd
+from pprint import pprint
+import argparse
+
+# Gensim
+import gensim, spacy, logging, warnings
+import gensim.corpora as corpora
+from gensim.utils import lemmatize, simple_preprocess
+from gensim.models import CoherenceModel
+import matplotlib.pyplot as plt
+
+# NLTK Stop words
+from nltk.corpus import stopwords
+stop_words = stopwords.words('english')
+stop_words.extend(['from', 'subject', 're', 'edu', 'use', 'not', 'would', 'say', 'could', '_', 'be', 'know', 'good', 'go', 'get', 'do', 'done', 'try', 'many', 'some', 'nice', 'thank', 'think', 'see', 'rather', 'easy', 'easily', 'lot', 'lack', 'make', 'want', 'seem', 'run', 'need', 'even', 'right', 'line', 'even', 'also', 'may', 'take', 'come'
+                  , 'today', 'great','new','https','com','love','thing','day'])
+
+
+warnings.filterwarnings("ignore",category=DeprecationWarning)
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.ERROR)
+
+
+parser = argparse.ArgumentParser(description = 'topic analysis')
+parser.add_argument('--inputfilename', type=str, default='defaultinput.json')
+parser.add_argument('--outputfilename', type=str, default='defaultoutput.json')
+parser.add_argument('--modelsavename', type=str, default='defaultmodelname.json')
+args = parser.parse_args()
+
+
+f = open(args.inputfilename)
+twitter = json.load(f)
+
+text = []
+id = []
+for i in range(len(twitter['docs'])):
+    text.append(twitter['docs'][i]['text'])
+    id.append(twitter['docs'][i]['id'])
+df = pd.DataFrame({'text':text, 'id': id})
+df
+
+
+#pip install spacy
+
+
+
+def sent_to_words(sentences):
+    for sent in sentences:
+        sent = re.sub('\S*@\S*\s?', '', sent)  # remove emails
+        sent = re.sub('\s+', ' ', sent)  # remove newline chars
+        sent = re.sub("\'", "", sent)  # remove single quotes
+        sent = gensim.utils.simple_preprocess(str(sent), deacc=True) 
+        yield(sent)  
+
+data_words = list(sent_to_words(df['text']))
+
+
+# Build the bigram and trigram models
+bigram = gensim.models.Phrases(data_words, min_count=5, threshold=100) # higher threshold fewer phrases.
+trigram = gensim.models.Phrases(bigram[data_words], threshold=100)  
+bigram_mod = gensim.models.phrases.Phraser(bigram)
+trigram_mod = gensim.models.phrases.Phraser(trigram)
+
+# !python -m spacy download en  # run in terminal once
+def process_words(texts, stop_words=stop_words, allowed_postags=['NOUN', 'ADJ']):
+    """Remove Stopwords, Form Bigrams, Trigrams and Lemmatization"""
+    texts = [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts]
+    texts = [bigram_mod[doc] for doc in texts]
+    texts = [trigram_mod[bigram_mod[doc]] for doc in texts]
+    texts_out = []
+    nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
+    for sent in texts:
+        doc = nlp(" ".join(sent)) 
+        texts_out.append([token.lemma_ for token in doc if token.pos_ in allowed_postags])
+    # remove stopwords once more after lemmatization
+    texts_out = [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts_out]    
+    return texts_out
+
+data_ready = process_words(data_words)  # processed Text Data!
+
+
+
+# Create Dictionary
+id2word = corpora.Dictionary(data_ready)
+
+# Create Corpus: Term Document Frequency
+corpus = [id2word.doc2bow(text) for text in data_ready]
+
+# Build LDA model
+lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
+                                           id2word=id2word,
+                                           num_topics=6, 
+                                           random_state=100,
+                                           update_every=1,
+                                           chunksize=100,
+                                           passes=100,
+                                           alpha='symmetric',
+                                           iterations=100,
+                                           per_word_topics=True)
+
+pprint(lda_model.print_topics())
+
+
+
+from matplotlib import pyplot as plt
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.colors as mcolors
+
+cols = [color for name, color in mcolors.TABLEAU_COLORS.items()]  # more colors: 'mcolors.XKCD_COLORS'
+
+cloud = WordCloud(stopwords=stop_words,
+                  background_color='white',
+                  width=800,
+                  height=560,
+                  max_words=30,
+                  colormap='tab10',
+                  color_func=lambda *args, **kwargs: cols[i],
+                  prefer_horizontal=1.0)
+
+topics = lda_model.show_topics(formatted=False)
+
+fig, axes = plt.subplots(3, 2, figsize=(10,10), sharex=True, sharey=True)
+
+for i, ax in enumerate(axes.flatten()):
+    fig.add_subplot(ax)
+    topic_words = dict(topics[i][1])
+    cloud.generate_from_frequencies(topic_words, max_font_size=300)
+    plt.gca().imshow(cloud)
+    plt.gca().set_title('Topic ' + str(i), fontdict=dict(size=16))
+    plt.gca().axis('off')
+
+plt.savefig(args.outputfilename)
+plt.subplots_adjust(wspace=0, hspace=0)
+plt.axis('off')
+plt.margins(x=0, y=0)
+plt.tight_layout()
+plt.show()
+
+
+lda_model.save(args.modelsavename)
+
+
+#testload = gensim.models.ldamodel.LdaModel.load("lda")
+
+
+#testload
+
+
+
+
+
+
