796c492b655d650425c3be8895e156db375ed68a
Peng Cao <45455020+c731615340@users.noreply.github.com>
2020-05-26 13:47:33 +1000

    Update label.py

--- analysis/label.py
+++ analysis/label.py
@@ -1,82 +1,76 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
+#import modules
 import json
 import json
-from ast import literal_eval
 import nltk
 from tqdm import tqdm
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-import sys
 import argparse
 
-
-parser = argparse.ArgumentParser(description = 'label the simple sentiment analysis')
-parser.add_argument('--inputfilename', type=str, default='defaultinput.json')
-parser.add_argument('--outputfilename', type=str, default='defaultoutput.json')
-args = parser.parse_args()
-
-#nltk.download('vader_lexicon')
-
-
-
-f = open(args.inputfilename)
-twitter = json.load(f)
-fr = twitter['docs']
-
-text = []
-for i in range(len(twitter['docs'])):
-    text.append(twitter['docs'][i]['text'])
-
-
-no_text_index = []
-for i in range(len(fr)):
-    if len(fr[i]['text']) == 0:
-        no_text_index.append(i)
-
-
-
-
-processed_fr = [i for i in fr if i['text']]
-
-
-
-
-len(processed_fr)
-
-
-
-(len(no_text_index)+len(processed_fr)) == len(fr)
-
-
-
+#define a function that gives score of a sentence, using simple analyzer in nltk.
 def nltk_sentiment(sentence):
     nltk_sentiment = SentimentIntensityAnalyzer()
     score = nltk_sentiment.polarity_scores(sentence)
     return score
 
-
-
-
-for i in tqdm(range(len(processed_fr))):
-    score = nltk_sentiment(processed_fr[i]['text'])
-    #processed_fr[i]['negative'] = score['neg']
-    #processed_fr[i]['neutral'] = score['neu']
-    #processed_fr[i]['positive'] = score['pos']
-    #processed_fr[i]['compound'] = score['compound']
+#define a function that given a tweet, gives a result from positive, negative or neutral
+def label_sentiment(tweet):
+    text = tweet['text']
+    score = nltk_sentiment(text)
     if score['compound'] >= 0.05:
-        processed_fr[i]['simple_sentiment_label'] = str("positive")
+        tweet['simple_sentiment_label'] = str("positive")
     elif score['compound'] <= -0.05:
-        processed_fr[i]['simple_sentiment_label'] = str("negative")
+        tweet['simple_sentiment_label'] = str("negative")
     else:
-        processed_fr[i]['simple_sentiment_label'] = str("neutral")
+        tweet['simple_sentiment_label'] = str("neutral")
+    return tweet
 
-twitter['docs'] = processed_fr
+if __name__ == '__main__':
+    
+    #add argument of input and output file name, with input being unlabeled tweets, and output being labeled.
+    parser = argparse.ArgumentParser(description = 'label the simple sentiment analysis')
+    parser.add_argument('--inputfilename', type=str, default='defaultinput.json')
+    parser.add_argument('--outputfilename', type=str, default='defaultoutput.json')
+    args = parser.parse_args()
 
 
-with open(args.outputfilename, 'w') as f:
-    json.dump(twitter, f)
+    f = open(args.inputfilename,'r+',encoding = "utf-8")
+    tweets = json.loads(f.read())["docs"]
+    
+    #add a process of detecting no-text tweets, and store their index
+    no_text_index = []
+    processed_fr = []
+    for candidate in tweets:
+        if len(candidate['text']) == 0:
+            no_text_index.append(candidate)
+        else:
+            processed_fr.append(candidate)
+
+    #label every tweets if they have text.
+    for i in tqdm(range(len(processed_fr))):
+        text = processed_fr[i]['text']
+        score = nltk_sentiment(text)
+        #processed_fr[i]['negative'] = score['neg']
+        #processed_fr[i]['neutral'] = score['neu']
+        #processed_fr[i]['positive'] = score['pos']
+        #processed_fr[i]['compound'] = score['compound']
+        if score['compound'] >= 0.05:
+            processed_fr[i]['simple_sentiment_label'] = str("positive")
+        elif score['compound'] <= -0.05:
+            processed_fr[i]['simple_sentiment_label'] = str("negative")
+        else:
+            processed_fr[i]['simple_sentiment_label'] = str("neutral")
+
+    result = {"docs":processed_fr}
+
+
+
+    with open(args.outputfilename, 'w+',encoding="utf-8") as fp:
+        json.dump(result, fp,indent= 2)
+
+    f.close()
 
 
 
